880a32a8f5518fa462216789ca58a2c93b7f2f58
timurxyz <x@secdev.eu>
2021-02-21 03:12:55 +0100

    -- JB auto-refactored to AndroidX -- build.gradle now use .+ versions

--- app/src/main/java/com/example/zsombor/activitystarterdemo/MainActivity.java
+++ app/src/main/java/com/example/zsombor/activitystarterdemo/MainActivity.java
@@ -1,9 +1,8 @@
 package com.example.zsombor.activitystarterdemo;
 
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
